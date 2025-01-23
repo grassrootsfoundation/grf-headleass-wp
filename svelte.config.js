2bf2df8edf87d0581b6b81026f2ba7b9cee10346
morgansegura <morgansegura@gmail.com>
2025-01-23 14:25:39 -0800

    Fix Azure Static Web Apps workflow

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import azure from 'svelte-adapter-azure-swa';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -13,7 +13,11 @@ const config = {
     // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-    adapter: azure(),
+    adapter: adapter({
+      pages: 'build', // Directory for static files
+      assets: 'build', // Directory for assets
+      fallback: 'index.html', // Optional for SPAs
+    }),
     prerender: {
       entries: ['*'], // Pre-render all routes
     },
