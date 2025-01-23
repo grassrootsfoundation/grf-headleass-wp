3a35a261e082dd7e6e4c0b90a94980dde5ad3cad
morgansegura <morgansegura@gmail.com>
2025-01-23 07:47:44 -0800

    Updating build for azure

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,9 @@ const config = {
     // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-    adapter: azure(),
+    adapter: azure({
+      output: '.svelte-kit/azure-tmp',
+    }),
     alias: {
       '$src/*': './src/*',
       '$lib/*': './src/lib/*',
