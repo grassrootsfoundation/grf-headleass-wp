3e116bd26116f1b1ce8c34e4b7cc97999442fb8e
morgansegura <morgansegura@gmail.com>
2025-01-23 08:38:23 -0800

    Updating build for azure

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,5 +1,13 @@
 import { MediaQuery } from '$utils/constants';
 
+export function handleError(error: Error) {
+  console.error('An error occurred:', error);
+}
+
+export function init() {
+  console.log('Client hooks initialized');
+}
+
 /**
  * Used as a convience method for accessing pre-defined
  * breakpoints. Uses `useMediaQuery` under the hood.
