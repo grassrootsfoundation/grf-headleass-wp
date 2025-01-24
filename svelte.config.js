1801f9d1815047e1a09cf23560071482d2af6628
morgansegura <morgansegura@gmail.com>
2025-01-24 07:51:50 -0800

    Add env variables to deploy config

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,14 @@ const config = {
     // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-    adapter: azure(),
+    adapter: azure({
+      pages: 'build',
+      assets: 'build',
+      fallback: undefined,
+      precompress: false,
+      strict: true,
+    }),
+    paths: { relative: false },
     prerender: {
       entries: ['*'], // Pre-render all routes
     },
